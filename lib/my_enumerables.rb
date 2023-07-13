67319c57d85006762651212d420973df79077968
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2023-07-13 10:40:50 +0100

    Custom enumberables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,5 @@
 module Enumerable
-  # Your code goes here
+
 end
 
 # You will first have to define my_each
@@ -8,4 +8,84 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_each
+    for el in self
+      yield el
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    for el in self
+      yield el, i
+      i += 1
+    end
+  end
+
+  def my_select
+    results = []
+
+    for el in self
+      results.append(el) if yield(el)
+    end
+
+    results
+  end
+
+  def my_all?
+    for el in self
+      return false if !yield(el)
+    end
+    true
+  end
+
+  def my_any?
+    for el in self
+      return true if yield(el)
+    end
+    false
+  end
+
+  def my_none?
+    for el in self
+      return false if yield(el)
+    end
+    true
+  end
+
+  def my_count
+    return length if !block_given?
+    count = 0
+    for el in self
+      count += 1 if yield(el)
+    end
+    count
+  end
+
+  def my_map
+    arr = []
+    for el in self
+      arr.append(yield(el))
+    end
+    arr
+  end
+
+  def my_inject(arg = nil)
+    if !block_given? && arg.is_a?(Symbol)
+      sum = nil
+      for el in self
+        sum.nil? ? sum = el : sum = sum.send(arg, el)
+      end
+    else
+      init = 0
+      init = arg if arg.is_a?(Integer)
+
+      for el in self
+        init = yield(init, el)
+      end
+
+      init
+    end
+  end
 end
